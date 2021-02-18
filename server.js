d19487e1120ce85ee50f1df62e474157ef3c5d05
AElSamman <43209096+AElSamman@users.noreply.github.com>
2021-02-18 07:37:51 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,38 +3,40 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
-const bodyParser = require('bodyParser');
+
+// Require Express to run server and routes
+const bodyParser = require('body-parser');
+
+// Require Express to run server and routes
 const cors = require('cors');
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+//Here we are configuring express to use body-parser as a middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website')); 
-
 
 // Setup Server
 app.listen(3000, () => console.log('Server Up and running on local host 3000'));
 
 // GET request
-app.get('/get', (req, res) => {
+app.get('/getdata', (req, res) => {
     res.send(projectData)
     }
 );
 
 // POST request
-app.post('/post', (req, res) => {
-    projectData["temperature"] = req.body.temperature
+app.post('/postdata', (req, res) => {
     projectData["date"] = req.body.date
+    projectData["temp"] = req.body.temp
     projectData["userResponse"] = req.body.userResponse
     res.send(projectData)
     }
--- website/app.js
+++ website/app.js
@@ -1,63 +1,66 @@
 /* Global Variables */
 
-const { request } = require("http");
-const { getHeapCodeStatistics } = require("v8");
-
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// Added +1 to the getMonth() method because it starts from zero 
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
-let baseUrl = 'api.openweathermap.org/data/2.5/weather?zip='
-let apiKey = '&appid=d5530d9fef2d5fbeb2f7cfae40f6d4f2';
+// We are using the following URL format and parameters to access current weather data for any location on Earth
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
-//Event Listening
+// This is my personal API key which is an application programming interface key is a unique identifier used to authenticate a user, developer, or calling program to an API
+const apiKey = '&appid=d5530d9fef2d5fbeb2f7cfae40f6d4f2';
+
+//The generate click event Listening
 document.getElementById('generate').addEventListener('click', (event) => {
-    console.log(event)
-    const zip = document.getElementById('zip').value;
-    const feelings = document.getElementById('feelings').value;
-    getData(baseUrl.apiKey,zip).then( (data) => {
-        console.log(data)
-        post('/post', {date:d, temp:data.list[0].main.temp, userResponse: feelings})
-        update();
-    })
+    const zip = document.getElementById('zip').value; // acquiring the zip code entered by the client
+    const feelings = document.getElementById('feelings').value; // acquiring the data entered by the client regarding the feeling question
+    getTemperatureData(baseUrl, zip, apiKey) // calling the getData function and passing the URL parameters
+        .then((temperatureData) => {
+            postData('/postdata', { date: newDate, temp: temperatureData, userResponse: feelings });
+            updateUserInterface();
+        })
 })
 
-// GET DATA
-const getData = async (baseUrl, apiKey, zip) => {
-    const res = await fetch(baseUrl+zip+apiKey)
-    try{
-        const data = await res.json();
-        return data
-    } catch(err){
-        console.log(err)
+// GET Temperature Data
+const getTemperatureData = async (baseUrl, zip, apiKey) => {
+    const response = await fetch(baseUrl + zip + apiKey)
+    try {
+        const temperatureData = await response.json(); // resolves to a JavaScript object with the result of parsing the body text as JSON
+        return temperatureData.main.temp; // obtain the exact temperature value 
+    } catch (error) {
+        console.error('Error:', error);
     }
 }
 
-//POST DATA
-const post = async (url = '', data = {}) => {
-    console.log(data);
-    const res = await fetch(url, {
-        method: 'POST',
+// POST Data
+const postData = async (url = '', projectData = {}) => {
+    const response = await fetch(url, {
+        method: 'POST', // The request method
+        mode: 'cors',
+        cache: 'no-cache',
         credentials: 'same-origin',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+        headers: { // Content-Type header tells us what the content type of the returned content actually is
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(projectData) // body data type must match "Content-Type" header
     })
-    try{
-        const data = await res.json();
-        console.log(data)
-        return data;
-    } catch(err){
-        console.log(err)
+    try {
+        return projectData = await response.json(); // It returns a JavaScript object that resolves with the result of parsing the body text as JSON
+    } catch (error) {
+        console.error('Error:', error);
     }
 }
 
-const update = async () => {
-    const req = await fetch('/get');
-    try{const data = await req.json();
-    document.getElementById('date').innerHTML = `Date: ${data[0].date}`;
-    document.getElementById('temp').innerHTML = `Date: ${data[0].temp}`;
-    document.getElementById('content').innerHTML = `Date: ${data[0].content}`;
-    }catch(err){
-        console.log(err)
+// UPDATE the user inerface
+const updateUserInterface = async () => {
+    const response = await fetch('/getdata')
+    try {
+        const projectData = await response.json();
+        document.getElementById('date').innerHTML = `Date: ${projectData.date}`;
+        document.getElementById('temp').innerHTML = `Temperature: ${projectData.temp}`;
+        document.getElementById('content').innerHTML = `I'm feeling ${projectData.userResponse}`;
+    } catch (error) {
+        console.error('Error:', error);
     }
 }
